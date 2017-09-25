9b46a5c931f132e17472ab0e08037f80d2f13acb
wuxingx <121@QQ.COM>
2017-09-25 09:23:35 +0800

    121

--- zdp/src/main/java/com/zdp/service/run.java
+++ zdp/src/main/java/com/zdp/service/run.java
@@ -1,13 +1,18 @@
 package com.zdp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
 public class run {
+
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[]{"application.xml"});
+        TestService testService = (TestService)context.getBean("testService");
+        testService.getMsg();
         context.start();
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
